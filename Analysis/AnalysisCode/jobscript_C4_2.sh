61e59d21ad9397de27b2833f5d542e6686842c78
idarose <idarosenqvist99@gmail.com>
2024-03-05 16:29:12 +0100

    Tests

--- Analysis/AnalysisCode/jobscript_C4_2.sh
+++ Analysis/AnalysisCode/jobscript_C4_2.sh
@@ -3,10 +3,10 @@
 #SBATCH --job-name AlphaTherapy
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=idapro@fys.uio.no
-#SBATCH --time=0-00:10:00
+#SBATCH --time=0-00:30:00
 #SBATCH --mem-per-cpu=15G
 #SBATCH --ntasks=1 --cpus-per-task=10
-#SBATCH --array=5
+#SBATCH --array=150
 ##SBATCH --array=1,3,5,10,25,50,75,100,150
 
 ##export DISPLAY=:0
