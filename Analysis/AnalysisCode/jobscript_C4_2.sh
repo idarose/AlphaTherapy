47a73728d4ae2abe4592cc5767dced19d01cb0c0
idarose <idarosenqvist99@gmail.com>
2024-03-05 22:02:34 +0100

    Tests

--- Analysis/AnalysisCode/jobscript_C4_2.sh
+++ Analysis/AnalysisCode/jobscript_C4_2.sh
@@ -4,7 +4,7 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=idapro@fys.uio.no
 #SBATCH --time=0-02:00:00
-#SBATCH --mem-per-cpu=15G
+#SBATCH --mem-per-cpu=4G
 #SBATCH --ntasks=1 --cpus-per-task=10
 #SBATCH --array=150
 ##SBATCH --array=1,3,5,10,25,50,75,100,150
