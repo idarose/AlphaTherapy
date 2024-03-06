d42992e46b42c6af614001a13abe62f4a1736656
idarose <idarosenqvist99@gmail.com>
2024-03-06 16:25:25 +0100

    Added new jobs

---
+++ Analysis/AnalysisCode/jobscript_PC3_PIP.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --account=NN9895K
+#SBATCH --job-name AlphaTherapy
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=idapro@fys.uio.no
+#SBATCH --time=0-20:00:00
+#SBATCH --mem-per-cpu=4G
+#SBATCH --ntasks=1 --cpus-per-task=40
+#SBATCH --array=1,3,5,10,25,50,75,100,150
+
+EXECUTABLE=main
+
+ACTIVITY=$SLURM_ARRAY_TASK_ID
+OUTPUTFILE="TerminalOutput_PC3_PIP_${ACTIVITY}kBq.txt"
+
+CELLLINE=PC3_PIP
+NUMBERITERATIONS=40
+
+
+./$EXECUTABLE $CELLLINE $ACTIVITY $NUMBERITERATIONS  > $OUTPUTFILE
