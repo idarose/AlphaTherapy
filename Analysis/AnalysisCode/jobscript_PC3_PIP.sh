3f9f7a44a43cd0b215f0be5077ecb185ac0abe42
idarose <idarosenqvist99@gmail.com>
2024-03-12 08:46:33 +0100

    Tests

--- Analysis/AnalysisCode/jobscript_PC3_PIP.sh
+++ Analysis/AnalysisCode/jobscript_PC3_PIP.sh
@@ -4,8 +4,8 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=idapro@fys.uio.no
 #SBATCH --time=1-10:00:00
-#SBATCH --mem-per-cpu=64G
-#SBATCH --ntasks=1 --cpus-per-task=1
+#SBATCH --mem-per-cpu=8G
+#SBATCH --ntasks=1 --cpus-per-task=20
 #SBATCH --array=150
 
 EXECUTABLE=main
@@ -14,7 +14,7 @@ ACTIVITY=$SLURM_ARRAY_TASK_ID
 OUTPUTFILE="TerminalOutput_PC3_PIP_${ACTIVITY}kBq.txt"
 
 CELLLINE=PC3_PIP
-NUMBERITERATIONS=40
+NUMBERITERATIONS=20
 
 
 ./$EXECUTABLE $CELLLINE $ACTIVITY $NUMBERITERATIONS  > $OUTPUTFILE
