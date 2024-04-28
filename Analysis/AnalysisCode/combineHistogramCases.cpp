616fa4f21eee73a96d378cd7f8f29ed747977e0f
idarose <idarosenqvist99@gmail.com>
2024-04-28 11:38:25 +0200

    :

--- Analysis/AnalysisCode/combineHistogramCases.cpp
+++ Analysis/AnalysisCode/combineHistogramCases.cpp
@@ -244,6 +244,184 @@ void CombineHistogramsPC3PIP(std::string cellGeometry, int activity)
 
 }
 
+void CombineHistogramsPC3Flu(std::string cellGeometry, int activity)
+{
+    // Open the ROOT file
+    std::string filename1 = "Output_" + cellGeometry + "/Output_PC3_Flu_" + std::to_string(activity) + "kBq_case_1.root";
+    std::string filename2 = "Output_" + cellGeometry + "/Output_PC3_Flu_" + std::to_string(activity) + "kBq_case_2.root";
+
+
+    TFile *file1 = TFile::Open(filename1.c_str(), "READ");
+
+    std::vector<TH1D*> vec1DHists_Case1;
+    std::vector<TH2D*> vec2DHists_Case1;
+
+
+    if (file1 && !file1->IsZombie()) {
+        TList* list = file1->GetListOfKeys();
+        TIter next(list);
+        TKey* key;
+
+        // Loop over all keys and retrieve objects
+        while ((key = (TKey*)next())) {
+            TObject* obj = key->ReadObj();  // Declare 'obj' here
+            TClass* cl = gROOT->GetClass(obj->ClassName());
+            if (!obj || !cl) {
+                delete obj;  // Guard against memory leak if obj is not null
+                continue;
+            }
+
+            if (cl->InheritsFrom("TH1D")) {
+                TH1D* histo = (TH1D*)obj->Clone(); // Clone the histogram
+                histo->SetDirectory(0);
+                vec1DHists_Case1.push_back(histo);
+            }
+            else if (cl->InheritsFrom("TH2D")) {
+                TH2D* histo2 = (TH2D*)obj->Clone(); // Clone the 2D histogram
+                histo2->SetDirectory(0);
+                vec2DHists_Case1.push_back(histo2);
+            }
+
+            // Ensure you delete the original object only when you are sure you will not use it anymore
+            delete obj;
+        }
+
+    }
+    else {
+        std::cerr << "Could not open file or file is corrupted: " << filename1 << std::endl;
+    }
+
+    file1->Close();
+
+
+    TFile *file2 = TFile::Open(filename2.c_str(), "READ");
+
+    std::vector<TH1D*> vec1DHists_Case2;
+    std::vector<TH2D*> vec2DHists_Case2;
+
+
+    if (file2 && !file2->IsZombie()) {
+        TList* list = file2->GetListOfKeys();
+        TIter next(list);
+        TKey* key;
+
+        // Loop over all keys and retrieve objects
+        while ((key = (TKey*)next())) {
+            TObject* obj = key->ReadObj();  // Declare 'obj' here
+            TClass* cl = gROOT->GetClass(obj->ClassName());
+            if (!obj || !cl) {
+                delete obj;  // Guard against memory leak if obj is not null
+                continue;
+            }
+
+            if (cl->InheritsFrom("TH1D")) {
+                TH1D* histo = (TH1D*)obj->Clone(); // Clone the histogram
+                histo->SetDirectory(0);
+                vec1DHists_Case2.push_back(histo);
+            }
+            else if (cl->InheritsFrom("TH2D")) {
+                TH2D* histo2 = (TH2D*)obj->Clone(); // Clone the 2D histogram
+                histo2->SetDirectory(0);
+                vec2DHists_Case2.push_back(histo2);
+            }
+
+            // Ensure you delete the original object only when you are sure you will not use it anymore
+            delete obj;
+        }
+
+    }
+    else {
+        std::cerr << "Could not open file or file is corrupted: " << filename1 << std::endl;
+    }
+
+    file2->Close();
+
+    for(int i=0; i<vec1DHists_Case2.size(); i++)
+    {
+        (vec1DHists_Case1[i])->Add(vec1DHists_Case2[i]);
+    }
+    for(int i=0; i<vec2DHists_Case2.size(); i++)
+    {
+        (vec2DHists_Case1[i])->Add(vec2DHists_Case2[i]);
+    }
+
+    vec1DHists_Case2.clear();
+    vec2DHists_Case2.clear();
+
+
+    std::string outputName = "Output_" + cellGeometry + "/Output_PC3_Flu_" + std::to_string(activity) + "kBq.root";
+    auto output = new TFile(outputName.c_str(), "RECREATE");
+
+    for(auto & entry : vec1DHists_Case1)
+    {
+        entry->Scale(1./2.);
+        entry->Write();
+    }
+    output->Write();
+    output->Close();
+
+}
+
+void CombineHistogramsC42(std::string cellGeometry, int activity)
+{
+    // Open the ROOT file
+    std::string filename1 = "Output_" + cellGeometry + "/Output_C4_2_" + std::to_string(activity) + "kBq_case_1.root";
+
+
+    TFile *file1 = TFile::Open(filename1.c_str(), "READ");
+
+    std::vector<TH1D*> vec1DHists_Case1;
+
+
+    if (file1 && !file1->IsZombie()) {
+        TList* list = file1->GetListOfKeys();
+        TIter next(list);
+        TKey* key;
+
+        // Loop over all keys and retrieve objects
+        while ((key = (TKey*)next())) {
+            TObject* obj = key->ReadObj();  // Declare 'obj' here
+            TClass* cl = gROOT->GetClass(obj->ClassName());
+            if (!obj || !cl) {
+                delete obj;  // Guard against memory leak if obj is not null
+                continue;
+            }
+
+            if (cl->InheritsFrom("TH1D")) {
+                TH1D* histo = (TH1D*)obj->Clone(); // Clone the histogram
+                histo->SetDirectory(0);
+                vec1DHists_Case1.push_back(histo);
+            }
+            else if (cl->InheritsFrom("TH2D")) {
+                TH2D* histo2 = (TH2D*)obj->Clone(); // Clone the 2D histogram
+                histo2->SetDirectory(0);
+                vec2DHists_Case1.push_back(histo2);
+            }
+
+            // Ensure you delete the original object only when you are sure you will not use it anymore
+            delete obj;
+        }
+
+    }
+    else {
+        std::cerr << "Could not open file or file is corrupted: " << filename1 << std::endl;
+    }
+
+    file1->Close();
+
+
+    std::string outputName = "Output_" + cellGeometry + "/Output_C4_2_" + std::to_string(activity) + "kBq.root";
+    auto output = new TFile(outputName.c_str(), "RECREATE");
+
+    for(auto & entry : vec1DHists_Case1)
+    {
+        entry->Scale(1./1.);
+        entry->Write();
+    }
+    output->Write();
+    output->Close();
+}
+
 int main(int argc, char *argv[]) {
 
     //------------------–----------
@@ -283,6 +461,8 @@ int main(int argc, char *argv[]) {
 
     // Call the function with the name of your ROOT file
     CombineHistogramsPC3PIP(cellGeometry, activity);
+    CombineHistogramsPC3Flu(cellGeometry, activity);
+    CombineHistogramsC42(cellGeometry, activity);
 
     return 0;
 }
